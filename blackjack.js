b9a42ce120f1755e40d0883000278e904d275202
Haidar Nasralla <haidar@DESKTOP-PMMCL0B.>
2025-01-14 22:07:45 +0000

    Added game restart functionality

--- blackjack.js
+++ blackjack.js
@@ -6,13 +6,13 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-const game = new Game();
+let game;
 
 function displayState() {
-  console.log(`Your cards: ${game.player.getHand().map(card => card.toString()).join(', ')}`);
-  console.log(`Your score: ${game.player.getScore()}\n`);
+  console.log(`* Your cards: ${game.player.getHand().map(card => card.toString()).join(', ')}`);
+  console.log(`* Your score: ${game.player.getScore()}\n`);
   const dealerHand = game.dealer.getHand();
-  console.log(`Dealer's visible card: ${dealerHand[0].toString()}\n`);
+  console.log(`* Dealer's visible card: ${dealerHand[0].toString()}\n`);
 }
 
 function askPlayerAction() {
@@ -50,23 +50,34 @@ function askPlayerAction() {
 
 function endGame() {
   console.log('FINAL RESULTS:\n');
-  console.log(`Your cards: ${game.player.getHand().map(card => card.toString()).join(', ')}`);
-  console.log(`Your score: ${game.player.getScore()}\n`);
-  console.log(`Dealer's cards: ${game.dealer.getHand().map(card => card.toString()).join(', ')}`);
-  console.log(`Dealer's score: ${game.dealer.getScore()}\n`);
+  console.log(`* Your cards: ${game.player.getHand().map(card => card.toString()).join(', ')}`);
+  console.log(`* Your score: ${game.player.getScore()}\n`);
+  console.log(`* Dealer's cards: ${game.dealer.getHand().map(card => card.toString()).join(', ')}`);
+  console.log(`* Dealer's score: ${game.dealer.getScore()}\n`);
 
   if (game.result === 'player_wins') {
     console.log('Congratulations! You win!');
   } else if (game.result === 'dealer_wins') {
-    console.log('House wins - too bad!');
+    console.log('Dealer wins - too bad!');
   } else if (game.result === 'tie') {
     console.log("It's a tie!");
   }
 
-  rl.close();
+  rl.question('\nPlay again? (y / n)', answer => {
+    if (answer.toLowerCase() === 'y') {
+      game = new Game();
+      console.log('\n------\n')
+      startGame();
+    } else {
+      console.log('\nThanks for playing!\n');
+      rl.close();
+    }
+  })
+    
 }
 
 function startGame() {
+  game = new Game();
   console.log('*** WELCOME TO BIG BUCKS CASINO! ***\n');
   console.log('*** Let\'s play Blackjack! ***');
   console.log('\n------\n');
