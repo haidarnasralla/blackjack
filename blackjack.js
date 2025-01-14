e3a01582f199c4beaebba65baa891619f9b91cb3
Haidar Nasralla <haidar@DESKTOP-PMMCL0B.>
2025-01-14 22:19:16 +0000

    More CLI tweaks

--- blackjack.js
+++ blackjack.js
@@ -69,7 +69,8 @@ function endGame() {
       console.log('\n------\n')
       startGame();
     } else {
-      console.log('\nThanks for playing! We hope this was informative, educational and entertaining.\n');
+      console.log('\n------\n')
+      console.log('Thanks for playing! We hope this was informative, educational and entertaining.\n');
       rl.close();
     }
   })
